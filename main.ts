fbbf047ad9254278a8d05cc74528d08c2437007b
just-nickname1 <wonjin120310@naver.com>
2025-07-15 10:33:12 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -3,3 +3,25 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     basic.clearScreen()
 })
+input.onGesture(Gesture.Shake, function () {
+    if (input.lightLevel() < 128) {
+        if (input.temperature() < 19) {
+            basic.showArrow(ArrowNames.South)
+        } else if (input.temperature() > 23) {
+            basic.showArrow(ArrowNames.North)
+        } else {
+            basic.showIcon(IconNames.Heart)
+        }
+    } else {
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.lightLevel() < 128) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+    basic.pause(500)
+    basic.clearScreen()
+})
